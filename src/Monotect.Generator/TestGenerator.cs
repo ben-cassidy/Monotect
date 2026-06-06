fdb0cc3aacdf41b678a7c40df604e1308728e5b1
Ben Cassidy <ben@bencassidy.net>
2026-06-06 13:44:22 +0100

    Reorder using statements to comply with formatting

--- src/Monotect.Generator/TestGenerator.cs
+++ src/Monotect.Generator/TestGenerator.cs
@@ -2,10 +2,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Text;
 using System;
 using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Monotect.Generator;
 
