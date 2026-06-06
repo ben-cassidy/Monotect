26630d4f2a2b3900211f80cbe10be84d8d147674
Ben Cassidy <ben@bencassidy.net>
2026-06-06 18:33:07 +0100

    Add test struct and struct analyzer

---
+++ src/Monotect.Analyzer/TestAnalyzer.cs
@@ -0,0 +1,59 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class TestAnalyzer : DiagnosticAnalyzer
+{
+    public static readonly DiagnosticDescriptor Rule = new(
+        id: "MT001",
+        title: "Don't use managed types in structs",
+        messageFormat: "Remove the managed type",
+        category: "ECS Functionality",
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
+
+    
+    private static void Analyze(SymbolAnalysisContext context)
+    {
+        // Get the type symbol of the type declaration
+        var type = (INamedTypeSymbol)context.Symbol;
+
+        // Only concerns structs
+        if (type.TypeKind != TypeKind.Struct)
+            return;
+
+        // Hypothetically we'd also check here that they implement IComponent
+
+        foreach (IFieldSymbol field in type.GetMembers().OfType<IFieldSymbol>())
+        {
+            // Skip compiler-generated boilerplate fields
+            if (field.IsImplicitlyDeclared)
+                continue;
+            
+            if (!field.Type.IsUnmanagedType)
+            {
+                Location location = field.Locations.FirstOrDefault()!;
+                context.ReportDiagnostic(Diagnostic.Create(Rule, location, field.Name, field.Type));
+            }
+        }
+
+    }
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        => ImmutableArray.Create(Rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.RegisterSymbolAction(Analyze, SymbolKind.NamedType);
+    }
+}
---
+++ src/Monotect/TestStruct.cs
@@ -0,0 +1,31 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using System;
+
+public unsafe struct TestStruct
+{
+    public int Value;
+
+    private string _managedField; // This should trigger the analyzer error
+
+    private int _unmanagedField; // This should be fine
+
+    private object _anotherManagedField; // This should also trigger the analyzer error
+
+    // array should trigger it
+    private int[] _managedArrayField;
+
+    // fixed buffer should be fine
+    private fixed int _unmanagedFixedBuffer[10];
+
+    // pointer should be fine
+    private int* _unmanagedPointerField;
+
+    // pointer to managed type should be fine
+    private string* _pointerToManagedTypeField;
+    
+
+
+}
